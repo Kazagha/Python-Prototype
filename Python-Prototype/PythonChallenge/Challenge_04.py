08a1ff8936f20bcec42f8eff81b498a457aacd7f
Kazagha <chrisg.local@gmail.com>
2017-04-19 21:36:33 +1000

    Challenge_04: Updated with more checks
    Updated to check three positions North, South, East and West.

--- Python-Prototype/PythonChallenge/Challenge_04.py
+++ Python-Prototype/PythonChallenge/Challenge_04.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     pass
 
 inputFileName = 'Challenge_04_Text.txt'
-result = 'this is a test'
+result = ''
 
 lineList = []
         
@@ -25,30 +25,43 @@ for lineNum in range(len(lineList)):
             
             # Check to the North of the current character 
             # Check that the current character is not in the first row
-            if(lineNum - 1 >= 0 and lineList[lineNum - 1][charNum].isupper()):
+            if(lineNum > 2 
+               and lineList[lineNum - 1][charNum].isupper()
+               and lineList[lineNum - 2][charNum].isupper()
+               and lineList[lineNum - 3][charNum].isupper()):
                 n = 1
             
             # Check to the South
-            if(lineNum + 1 < len(lineList) and lineList[lineNum + 1][charNum].isupper()):
+            if(lineNum < len(lineList) - 3 
+               and lineList[lineNum + 1][charNum].isupper()
+               and lineList[lineNum + 2][charNum].isupper()
+               and lineList[lineNum + 3][charNum].isupper()):
                 s = 1
         
+            
             # Check to the East
-            if(charNum + 1 <= 80 and lineList[lineNum][charNum + 1].isupper()):
+            if(charNum < 79 
+               and lineList[lineNum][charNum + 1].isupper()
+               and lineList[lineNum][charNum + 2].isupper()
+               and lineList[lineNum][charNum + 3].isupper()):
                 e = 1
                               
             # Check to the West
-            if(charNum - 1 >= 0 and lineList[lineNum][charNum - 1].isupper()):
+            if(charNum > 2 
+               and lineList[lineNum][charNum - 1].isupper()
+               and lineList[lineNum][charNum - 2].isupper()
+               and lineList[lineNum][charNum - 3].isupper()):
                 w = 1
             
             # Print the checks                                    
             #print(n,s,e,w)  
                                                 
             # Check if 3/4 checks have been successful 
-            if(n + s + e + w == 3):   
+            if(n + s + e + w == 4): 
                 # Append the character to the results string
                 result = result + currChar
-                print(currChar)
-                   
+                print(n,s,e,w)  
+                #print(currChar,lineList[lineNum][charNum - 1],lineList[lineNum][charNum - 3],lineList[lineNum][charNum - 3])                   
 # Print the result
 print('Length: ',len(result), 'Result: ', result)
 
