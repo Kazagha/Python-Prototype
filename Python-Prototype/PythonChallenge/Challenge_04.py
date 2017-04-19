bdaf78568f96029f8b3a686bfad681a430116609
Kazagha <chrisg.local@gmail.com>
2017-04-19 20:18:34 +1000

    Challenge_04: Initial commit
    Check if a lower case character is surrounded by at least three upper
    case characters. Begin implementation, start by reading the text from
    the file into a list.

---
+++ Python-Prototype/PythonChallenge/Challenge_04.py
@@ -0,0 +1,30 @@
+from test.test_decimal import file
+
+if __name__ == '__main__':
+    pass
+
+inputFileName = 'Challenge_04_Text.txt'
+
+lineList = []
+        
+with open(inputFileName) as f:
+    for line in f:
+            lineList.append(line)
+            
+#print(lineList[5], ' - ', len(lineList))
+
+for lineNum in range(len(lineList)):
+    for charNum in (range(len(lineList[lineNum]))):
+        currChar = lineList[lineNum][charNum]
+        
+        if(currChar.islower()):
+            print('Lower case character')
+             
+            n,s,e,w = 0,0,0,0
+            
+            # Check to the North of the current character 
+            # Check that the current character is not in the first row
+            if(lineNum - 1 >= 0 and lineList[lineNum - 1][charNum].islower()):
+                n = 1
+            
+          
