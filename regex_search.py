267750db88e3e2b0ef3a177c0ce906d20b840259
Kazagha <chrisg.local@gmail.com>
2017-05-17 19:42:14 +1000

    regex_search: Initial commit
    Outline of the required code.

---
+++ regex_search.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+import re
+
+Match = namedtuple('Match','file_name,search_term,result')
+
+def _load_files_in_dir(directory):
+    pass
+
+def _search(text):
+    pass
+
+def _write_to_csv(data ,filename):
+    pass
+
+if __name__ == '__main__':
+    pass
