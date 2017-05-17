51b0b085d746e4e21ff08b6910709e536d97a170
Kazagha <chrisg.local@gmail.com>
2017-05-17 21:52:46 +1000

    regex_search: Implement _write_to_csv method
    Write the results of the regex search to a CSV file

--- regex_search.py
+++ regex_search.py
@@ -3,6 +3,7 @@ import re
 import sys
 import os
 import glob
+import csv
 
 Match = namedtuple('Match','file_name,search_term,result')
 
@@ -16,26 +17,35 @@ def _load_files_in_dir(directory):
 def _search_files(files, REGEX):
     for f in files:
         search_result = REGEX.findall(f.result)
-        print(REGEX.findall(f.result))
+        #print(f.result, REGEX.findall(f.result))
         if(len(search_result) > 0):
-            yield Match(f.file_name, REGEX, search_result)
+            for result in search_result:
+                yield Match(f.file_name, REGEX, result)
 
 def _search_text(text):
     pass
 
 def _write_to_csv(data ,filename):
-    pass
+    with open(f'{filename}', 'w', newline='', encoding='utf8') as csvfile:
+        field_names = Match._fields
+
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+
+        writer.writeheader()
+        for match in data:
+            #print(match)
+            writer.writerow({'file_name':match.file_name,'search_term':match.search_term,'result':match.result})
 
 def search_files(search_dir, regex, output=None):
     files = _load_files_in_dir(search_dir)
-    #REGEX = re.compile(r'Application_Tasks.(Task_Code|Task_Description)')
-    REGEX = re.compile(r'Application_Task\.Task_Description.{0,3}=.{0,2}".+?"')
-    #print('Search' , REGEX.findall(r'Application_Task.Task_Description = "Test"'))
+    REGEX = re.compile(r'Application_Tasks\.(Task_Description|Task_Code).{0,3}=.{0,2}".+?"')
 
     results = _search_files(files, REGEX)
 
-    for r in results:
-        print (r)
+    #for r in results:
+    #    print (r)
+
+    _write_to_csv(results, output)
 
 
 if __name__ == '__main__':
@@ -43,4 +53,4 @@ if __name__ == '__main__':
     search_dir = 'C:\\temp\\Access'
     regex = ''
 
-    search_files(search_dir=search_dir, regex=regex)
+    search_files(search_dir=search_dir, regex=regex, output=r'C:\temp\outfile.csv')
