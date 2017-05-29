0d6b331d05e2c8b5da59a234c019f8b1d669529d
Kazagha <chrisg.local@gmail.com>
2017-05-29 14:20:45 +1000

    Pybites Spider: Older posts
    Automatically locate the 'older posts' button, browse to the page and
    continue to scrape articles.

--- scrapy_test/scrapy_test/spiders/pybites_spider.py
+++ scrapy_test/scrapy_test/spiders/pybites_spider.py
@@ -3,15 +3,26 @@ import scrapy
 class PybitesSpider(scrapy.Spider):
     name = "pybites_spider"
     start_urls = [
-        'http://pybit.es/'
+        'http://pybit.es/',
+        'http://pybit.es/index2.html'
     ]
 
     def parse(self, response):
         for article in response.css('article'):
             yield {
-                'title' : article.css('h2 a::text').extract(),
+                'title' : article.css('h2 a::text').extract_first(),
                 'tags'  : article.css('a~ a+ a::text').extract()
             }
+
+        self.log('Finished scraping articles')
+
+        next_page = response.css('.btn:nth-child(1)::attr(href)').extract_first()
+        if next_page is not None:
+            # In the example the URL is build using this command
+            # However the full URL is present here
+            #next_page = response.urljoin(next_page)
+            self.log(f'Moving to the next page {next_page}')
+            yield scrapy.Request(next_page, callback=self.parse)
 
 if __name__ == '__main__':
     pass
@@ -24,3 +35,6 @@ if __name__ == '__main__':
 
 # Extract Tags
 # article.css('a~ a+ a::text').extract()
+
+# Older Posts Button
+# response.css('.btn:nth-child(1)::attr(href)').extract()
