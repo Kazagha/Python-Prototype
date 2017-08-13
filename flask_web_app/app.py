f52ed3c96b4e41eba2bda06d27452f22eec57442
Kazagha <chrisg.local@gmail.com>
2017-08-13 20:32:16 +1000

    app: Outline app routes
    Basic outline of app routes without implementation.

--- flask_web_app/app.py
+++ flask_web_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from collections import defaultdict
 
 # persistence
 #
@@ -19,10 +20,45 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("template.html", data={'Chris':1234, 'Nick':5678}, col1='Name', col2='Phone Numbers' )
+    return render_template("template.html", data=get_lib(), col1='Select activity', col2='',
+                           activities=get_activities())
 
+@app.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html')
+
+@app.route('/sign_in')
+def sign_in():
+    return render_template("sign_up.html")
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    print(f'Submitting data')
+
+    _name = request.form['inputName']
+
+    #return render_template('sign_up.html')
+
+
+def get_lib():
+    """Return a basic library
+    
+    In the HTML template use the following syntax
+    {% for k, v in data.items() %}
+    """
+
+    return {'Chris':1234, 'Nick':5678}
+
+def get_activities():
+     return ['Buying','Carousing','Crafting','Crime','Gambling','Relaxation']
+
+def get_selected():
+    pass
+
+def get_history():
+    pass
 
 if __name__ == '__main__':
-    for i in range(1,10):
-        print (i)
+    for item in get_activities():
+        print(item)
     app.run(debug=True)
